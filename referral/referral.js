4363153857defe009da57d6109b5b41a0ce30a53
Yash Paliwal <yash@roverparking.com>
2016-07-21 14:02:29 -0400

    updated referral page, added logo img, etc

--- referral/referral.js
+++ referral/referral.js
@@ -1,15 +1,16 @@
 $(document).ready(function() {
 	var addFormInputs 
 		= '<div class="theForm row">'
-		+ '<p class="red col-md-2 col-md-offset-2 text-left">FRIEND\'S EMAIL:</p>'
-		+ '<input class="col-md-4 formInput" type="text" placeholder="email address">'
+		+ '<p class="red col-md-2 text-left">FRIEND\'S EMAIL:</p>'
+		+ '<input class="col-md-3 formInput" type="text" placeholder="email address">'
+		+ '<p class="red col-md-2 col-md-offset-1 text-left">FRIEND\'S EMAIL:</p>'
+		+ '<input class="col-md-3 formInput" type="text" placeholder="email address">'
 		+ '</div>';
 
+
+
 	$(".addMore").click(function() {
-		for (var i = 0; i < 5; i++) {
-			$("#refFormInputs").append(addFormInputs);	
-		}
-		
+		$("#refFormInputs").append(addFormInputs);
 	});
 	
 
