1e213c0a770413c80aafd6b1a1fb4968342eb09b
Yash Paliwal <yash@roverparking.com>
2016-07-21 14:12:52 -0400

    referral invite button fixed

--- referral/referral.js
+++ referral/referral.js
@@ -1,10 +1,8 @@
 $(document).ready(function() {
 	var addFormInputs 
 		= '<div class="theForm row">'
-		+ '<p class="red col-md-2 text-left">FRIEND\'S EMAIL:</p>'
-		+ '<input class="col-md-3 formInput" type="text" placeholder="email address">'
-		+ '<p class="red col-md-2 col-md-offset-1 text-left">FRIEND\'S EMAIL:</p>'
-		+ '<input class="col-md-3 formInput" type="text" placeholder="email address">'
+		+ '<p class="col-md-2 col-md-offset-3 col-sm-8 col-sm-offset-2 col-xs-12 red text-left">FRIEND\'S EMAIL:</p>'
+		+ '<input  class="col-md-3 col-md-offset-1 col-sm-8 col-sm-offset-2 col-xs-10 col-xs-offset-1 formInput" type="text" placeholder="email address">'
 		+ '</div>';
 
 
